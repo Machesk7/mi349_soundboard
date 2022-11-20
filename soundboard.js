37a70eaed5c90f0ebe60b6524e3308d04a845138
Jack Machesky <machesk7@msu.edu>
2022-11-20 17:30:37 -0500

    updated

--- soundboard.js
+++ soundboard.js
@@ -1,27 +1,11 @@
-const sounds = ['applause', 'boing', 'crash']
+const sounds = ['applause', 'boing', 'crash', ];
 
-sounds.forEach(sound => {
-    const btn = document.createElement('button')
-    btn.classList.add('btn')
+sounds.forEach((sound) => {
+    const btn = document.createElement('button');
+    btn.classList.add('btn');
 
-    btn.innerText = sound
-
-    btn.addEventListener('click', ()=> {
-        stopSongs()
-
-        document.getElementById(sound).onplay()
-    })
+    btn.innertext = sound;
 
     document.getElementById('buttons').appendChild(btn);
+    
 });
-
-funtion stopSongs () {
-    sounds.forEach(sound => {
-        const song = document.getElementById(sound)
-        
-        song.pause()
-        song.currentTime = 0;
-    })
-}
-
-
