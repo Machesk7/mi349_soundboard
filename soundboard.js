10e7d2e149d80ddfdc0760db974ba254a3cb85cc
Jack Machesky <machesk7@msu.edu>
2022-11-20 17:16:45 -0500

    updated

--- soundboard.js
+++ soundboard.js
@@ -1,8 +1,8 @@
 const sounds = ['applause', 'boing', 'crash'];
 
 sounds.forEach((sound) => {
-const btn = document.createElement('button')
-btn.classList.add('btn')
+const btn = document.createElement('button');
+btn.classList.add('btn');
 
 btn.innerText = sound;
 
